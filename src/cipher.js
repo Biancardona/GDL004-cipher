2c5d15f7b2d844af0aacd73ca06fef2dcba89ef9
biancardona <biancardona.rivera@gmail.com>
2019-10-29 09:11:17 -0600

    formula cipher encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,20 @@
 window.cipher = {
-  // Acá escribe tu código.
+  let encode = function(offset, string) {
+    let i;
+    //Declarando variable para concatenar caracteres
+    let encodedString = "";
+    //Recorrer cadena
+    for (i = 0; i < string.length; i++) {
+      //Obteniendo codigo ascii 
+      let asciiCode = string.charCodeAt(i);
+      let encoded = ((asciiCode - 65 + offset) % 26) + 65;
+      //Convirtiendo codigo ascci a string
+      let character = String.fromCharCode(encoded);
+      //Concatenando caracter
+      encodedString = encodedString.concat(character);
+    }
+    return encodedString;
+
+  }
+
 };
---
+++ src/index.js
@@ -0,0 +1,78 @@
+let elementoPadre1=
+document.querySelector(".inputDiv.il");
+let inputsRy = [];
+
+function Input() {
+  this.att ={};
+  this.att.type = "range";
+  this.att.value = any;
+  this.att.min = any;
+  this.att.max = any;
+  this.att.autocomplete = "off";
+  this.att.step = "1";
+  this.input;
+  this.output;
+}
+  this.crear = function(elementoPadre) {
+    // crea un nuevo elemento input
+    this.input = document.createElement("input");
+    //para cada propiedad del objeto att establece un nuevo atributo del elemento input
+    for (var name in this.att) {
+      if (this.att.hasOwnProperty(name)) {
+        this.input.setAttribute(name, this.att[name]);
+      }
+    }
+    // crea un nuevo elemento div
+    this.output = document.createElement("div");
+    // establece el valor del atributo class del nuevo div
+    this.output.setAttribute("class", "output");
+    // y el contenido (innerHTML) de este
+    this.output.innerHTML = this.att.value;
+
+    // inserta los dos elementos creados al final  del elemento Padre 
+    elementoPadre.appendChild(this.input);
+    elementoPadre.appendChild(this.output);
+  }
+
+  this.actualizar = function() {
+    this.output.innerHTML = this.input.value;
+    this.att.value = this.input.value;
+  }
+}
+
+// setup
+var i = new Input();
+i.crear(elementoPadre1);
+inputsRy.push(i);
+
+var i2 = new Input();
+i2.att.value = 70;
+i2.att.min = 20;
+i2.att.max = 120;
+i2.crear(elementoPadre2);
+inputsRy.push(i2);
+
+for (var n = 0; n < inputsRy.length; n++) {
+  (function(n) {
+    inputsRy[n].input.addEventListener("input", function() {
+      inputsRy[n].actualizar();
+    }, false)
+  }(n));
+}
+
+/* Draw
+function Draw(){
+ requestId = window.requestAnimationFrame(Draw); 
+  for( var n = 0; n< inputsRy.length; n++){
+    inputsRy[n].update();
+  }
+}
+
+requestId = window.requestAnimationFrame(Draw);
+*/
+// JavaScript Document
+
+Resources
+
+    fixedValue.
+}
