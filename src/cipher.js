1b9e71e0369abfe615b66bb5ef99cd03abf978a5
biancardona <biancardona.rivera@gmail.com>
2019-11-05 09:19:41 -0600

    correct function encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,21 +1,37 @@
 window.cipher = {
-  function (offset, string) {
+  encode (offset, string) {
     let i;
     //Declarando variable para concatenar caracteres
-    let encodedString = "";
+    let encodedString = ""; //Cadena vacia
     //Recorrer cadena
-    for (i = 0; i < string.length; i++) {
-      //Obteniendo codigo ascii 
+    for (i = 0; i < string.length; i++) { //Ciclo para cada caracter
+      //Obteniendo codigo ascii de cada caracter
       let asciiCode = string.charCodeAt(i);
-      let encoded = ((asciiCode - 65 + offset) % 26) + 65;
-      //Convirtiendo codigo ascci a string
+      let encoded = ((asciiCode - 65 + parseInt(offset)) % 26) + 65;
+      //Convirtiendo codigo ascii a string
       let character = String.fromCharCode(encoded);
       //Concatenando caracter
       encodedString = encodedString.concat(character);
     }
     return encodedString;
-
+  },
+   decode1 (offset, string) {
+    let i;
+    //Declarando variable para concatenar caracteres
+    let decodedString = ""; //Cadena vacia
+    //Recorrer cadena
+    for (i = 0; i < string.length; i++) { //Ciclo para cada caracter
+      //Obteniendo codigo ascii de cada caracter
+      let asciiCode = string.charCodeAt(i);
+      let decoded = ((asciiCode - 65 + parseInt(offset)) % 26) - 65;
+      //Convirtiendo codigo ascii a string
+      let character = String.fromCharCode(decoded);
+      //Concatenando caracter
+      decodedString = decodedString.concat(character);
+    }
+    return decodedString;
   }
-
 };
+
+
 
--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-let populateTable = function (string, table_id) {
+/*let populateTable = function (string, table_id) {
   let res = string.split("");
   let current_table = document.getElementById(table_id);
   for (var i = 0; i < string.lenght(); i++) {
     let new_td = document.createElement("td");
     new_td.innerHTML = res[i];
-    current_table.appendChild(new_td);
+    current_table.appendChild(new_td); 
   }
-};
+};*/
 //Funcion generica para esconder elementos por su ID
 let hide = function (elementId) {
   let elem = document.getElementById(elementId);
   elem.style.display = "none";
 };
-
+//Funcion generica para mostrar elementos por su ID
 let display = function (elementId) {
   let elem = document.getElementById(elementId);
   elem.style.display = "block";
 };
-
-
 //Funcion para remover y agregar elemento
 let el = document.getElementById("start");
 el.addEventListener("click", function () {
@@ -27,16 +25,25 @@ el.addEventListener("click", function () {
 el.addEventListener("click", function () {
   display("screen2");
 });
-
-/*
-let displayScreen2 = function () {
-  let new_div = document.createElement("div");
-  new_div.innerHTML = '<input id="text1" class="button" name="text1" ></input>';
-  document.getElementById("screen2").appendChild(new_div);
+//Funcion para obtener elemento de textarea encode
+let textAreaValue =function () {
+  let textArea = document.getElementById("encodetextarea").value;
+  textArea= textArea.toUpperCase();
+  let offsetNumber = document.getElementById("offset2").value;
+  let result = window.cipher.encode (offsetNumber,textArea);
+  document.getElementById("decodetextarea").value = result;
 };
+//Funcion para copiar el texto Encode a Decode
+document.getElementById("encodebutton").addEventListener("click", textAreaValue);
 
-let remove = function (elementId) {
-  let elem = document.getElementById(elementId);
-  elem.parentNode.removeChild(elem);
+//Funcion para obtener elemento de textarea decode
+let textAreaValue1 =function () {
+  let textArea1 = document.getElementById("decodetextarea").value;
+  textArea1= textArea1.toUpperCase();
+  let offsetNumber = document.getElementById("offset2").value;
+  let result = window.cipher.decode1 (offsetNumber,textArea1);
+  document.getElementById("encodetextarea").value = result;
 };
-*/
+//Funcion para copiar el texto Decode1 a Encode
+document.getElementById("decodebutton").addEventListener("click", textAreaValue1);
+
