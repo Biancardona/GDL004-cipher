42d8952da4069a7c1988d423a6aed06a36ae3f4f
biancardona <biancardona.rivera@gmail.com>
2019-11-10 19:21:17 -0600

    Final fixes

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,5 @@
 window.cipher = {
-  encode (offset, string) {
+  encode(offset, string) {
     let i;
     //Declarando variable para concatenar caracteres
     let encodedString = ""; //Cadena vacia
@@ -8,43 +8,45 @@ window.cipher = {
       //Obteniendo codigo ascii de cada caracter
       let asciiCode = string.charCodeAt(i);
       //Separando caracteres
-      if ((asciiCode >=65)&&(asciiCode<=90)){
-      //Formula para obtener el residuo del codigo ascci
-      let encoded = ((asciiCode - 65 + parseInt(offset)) % 26) + 65;
-      //Convirtiendo codigo ascii a string
-      let character = String.fromCharCode(encoded);
-      //Concatenando caracter
-      encodedString = encodedString.concat(character);
-    } else if ((asciiCode >= 97) && (asciiCode <= 122)) {
-      let encoded = ((asciiCode - 97 + parseInt(offset)) % 26) + 97;
-      //Convirtiendo codigo ascii a string
-      let character = String.fromCharCode(encoded);
-      //Concatenando caracter
-      encodedString = encodedString.concat(character);
-      }else{
-        encodedString=encodedString.concat(string[i]);
+      if ((asciiCode >= 65) && (asciiCode <= 90)) {
+        //Formula para obtener el residuo del codigo ascci
+        let encoded = ((asciiCode - 65 + parseInt(offset)) % 26) + 65;
+        //Convirtiendo codigo ascii a string
+        let character = String.fromCharCode(encoded);
+        //Concatenando caracter
+        encodedString = encodedString.concat(character);
+      } else if ((asciiCode >= 97) && (asciiCode <= 122)) {
+        let encoded = ((asciiCode - 97 + parseInt(offset)) % 26) + 97;
+        //Convirtiendo codigo ascii a string
+        let character = String.fromCharCode(encoded);
+        //Concatenando caracter
+        encodedString = encodedString.concat(character);
+      } else {
+        encodedString = encodedString.concat(string[i]);
+      }
     }
-  }
     return encodedString;
   },
-   decode (offset, string) {
-     let i;
-     let decodedString ="";
-     for (i = 0; i < string.length; i++) {
+  decode(offset, string) {
+    offset = parseInt(offset);
+    let i;
+    let decodedString = "";
+    for (i = 0; i < string.length; i++) {
       let asciiCode = string.charCodeAt(i);
-      if (asciiCode >=65 && asciiCode<=90){
-      let decoded = ((asciiCode + 65 - parseInt(offset)) % 26) + 65;
-      //Convirtiendo codigo ascii a string
-      let character = String.fromCharCode(decoded);
-      //Concatenando caracter
-      decodedString = decodedString.concat(character);
-    } else if ((asciiCode >= 97) && (asciiCode <= 122)) {
-      let decoded = ((asciiCode + parseInt(offset + 97)) % 26) + 97;
-      let character= String.fromCharCode(decoded);
-      decodedString = decodedString.concat(character);
-      }else{
-        decodedString=decodedString.concat(string[i]);
-}
-     }
+      if (asciiCode >= 65 && asciiCode <= 90) {
+        let decoded = ((asciiCode + 65 - parseInt(offset)) % 26) + 65;
+        //Convirtiendo codigo ascii a string
+        let character = String.fromCharCode(decoded);
+        //Concatenando caracter
+        decodedString = decodedString.concat(character);
+      } else if ((asciiCode >= 97) && (asciiCode <= 122)) {
+        let decoded = ((asciiCode + offset + 97) % 26) + 97;
+        let character = String.fromCharCode(decoded);
+        decodedString = decodedString.concat(character);
+      } else {
+        decodedString = decodedString.concat(string[i]);
+      }
+    }
     return decodedString;
-}};
+  }
+};
